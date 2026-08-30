ac4952afc1d406bca07ed76dffb6eb3ec45bf0cb
xkiet0301 <datnguyen123456ne@gmail.com>
2026-08-30 10:44:39 +0700

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,1 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := loader
+LOCAL_SRC_FILES := main.cpp
+LOCAL_LDLIBS := -llog -landroid -lGLESv2 -lEGL
+
+include $(BUILD_SHARED_LIBRARY)
 
