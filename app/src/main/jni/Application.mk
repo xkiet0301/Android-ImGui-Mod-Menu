22f5708f8b129dd1ed960a06fcafc3c408023c11
xkiet0301 <datnguyen123456ne@gmail.com>
2026-08-30 11:09:38 +0700

    Create Application.mk

---
+++ app/src/main/jni/Application.mk
@@ -0,0 +1,10 @@
+# Hỗ trợ cả 2 kiến trúc 32-bit và 64-bit phổ biến trên Android hiện nay
+APP_ABI := armeabi-v7a arm64-v8a
+
+APP_PLATFORM := android-21
+
+APP_STL := c++_static
+
+APP_CPPFLAGS := -std=c++17
+
+APP_OPTIM := release
