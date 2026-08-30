710f3c3937a7126ff3ea38ac038e880d3eff9ee8
xkiet0301 <datnguyen123456ne@gmail.com>
2026-08-30 10:51:22 +0700

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,12 +3,16 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := loader
 
-# Liệt kê tất cả các file nguồn C++ cần biên dịch
+# Tự động tìm tất cả các file .cpp trong thư mục jni và các thư mục con
 LOCAL_SRC_FILES := main.cpp
 
-# Thêm đường dẫn thư mục chứa file header (.h) nếu chúng nằm trong thư mục con
-# Ví dụ: Nếu thư mục 'Include' hoặc 'Main' chứa file .h, hãy thêm dòng dưới:
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
+# Nếu có thêm các file nguồn ở thư mục khác, hãy liệt kê vào đây, ví dụ:
+# ../Loader/loader.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+                    $(LOCAL_PATH)/Include \
+                    $(LOCAL_PATH)/Loader \
+                    $(LOCAL_PATH)/Main
 
 LOCAL_LDLIBS := -llog -landroid -lGLESv2 -lEGL
 
